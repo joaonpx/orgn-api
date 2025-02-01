9834f0ef11f5efaf29c3e038cef4b7de983e5512
Npx <cheyprivado@gmail.com>
2025-02-01 17:45:20 -0300

    iniciando projeto

---
+++ src/main/java/br/com/orgn/OrgnApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.orgn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgnApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OrgnApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/br/com/orgn/OrgnApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.orgn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrgnApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
