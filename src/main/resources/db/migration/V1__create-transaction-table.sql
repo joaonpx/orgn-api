3b3b047b5e0d5d2c50c64bbb23be0e8409ab2a69
Npx <cheyprivado@gmail.com>
2025-02-02 16:56:43 -0300

    Adicionando geração automatica de UUID e validação do campo amount

--- src/main/resources/db/migration/V1__create-transaction-table.sql
+++ src/main/resources/db/migration/V1__create-transaction-table.sql
@@ -1,7 +1,9 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE transaction (
-                             id UUID PRIMARY KEY NOT NULL,
+                             id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
                              date DATE NOT NULL,
                              type VARCHAR(255) NOT NULL,
                              description VARCHAR(255) NOT NULL,
-                             amount NUMERIC NOT NULL
+                             amount NUMERIC(10, 2) NOT NULL
 );
